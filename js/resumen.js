791a24df7e1c20a991777a44a2ad3f75fe22b1f5
CulturaEsquel <infoculturalesquel@gmail.com>
2025-06-19 20:07:45 -0300

    Update resumen.js

--- js/resumen.js
+++ js/resumen.js
@@ -2,7 +2,7 @@ fetch('resumen.txt')
   .then(response => response.text())
   .then(data => {
     const eventos = data.trim().split(/(?:\r?\n){2,}/);
-    let html = '<div class="p-1"><h2>Próximos Eventos</h2>';
+    let html = ;
 
     eventos.forEach(evento => {
       const lineas = evento.split('\n');
